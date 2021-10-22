0ae96276d3eae65b47b0591e61e35ee9213702d2
Andres <61568128+AndresGarciaU@users.noreply.github.com>
2021-10-22 18:39:15 -0500

    correcciones

--- springboot-mongodb/src/main/java/co/edu/poli/mongodb/SpringbootMongodbApplication.java
+++ springboot-mongodb/src/main/java/co/edu/poli/mongodb/SpringbootMongodbApplication.java
@@ -1,13 +1,17 @@
 package co.edu.poli.mongodb;
 
 import org.springframework.boot.SpringApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringbootMongodbApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(SpringbootMongodbApplication.class, args);
+		
 	}
 
 }
