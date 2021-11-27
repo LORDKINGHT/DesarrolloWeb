b4c108cf8fec425f3f1748004aa0810bcd0c582f
Andres <61568128+AndresGarciaU@users.noreply.github.com>
2021-11-26 19:40:55 -0500

    back+front

--- README.md
+++
@@ -1,2 +0,0 @@
-# DesarrolloWeb
-Basuras
--- springboot-mongodb/src/main/java/co/edu/poli/mongodb/services/MailService.java
+++
@@ -1,9 +0,0 @@
-package co.edu.poli.mongodb.services;
-
-import co.edu.poli.mongodb.model.Mail;
-
-public interface MailService {
-
-	public void sendEmail (Mail mail);
-	
-}
